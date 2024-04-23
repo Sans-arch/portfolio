38d070831b7cad8a9a8cb5aaf0ac8e10737ce8e2
Santiago Fleitas <sansfleitas@gmail.com>
2024-04-23 00:19:44 -0300

    Remove /

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: "https://sans-arch.github.io/",
+  site: "https://sans-arch.github.io",
 });
